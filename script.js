be9034be22e0af7340b006179beb63abb0dfa3fe
veronicalimm99-boop <veronicalimm99@gmail.com>
2026-06-20 10:25:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@
                 gap: 8px !important;
                 display: flex !important;
                 width: max-content !important;
-                animation: bankScrollLoop 18s linear infinite !important;
+                animation: bankScrollLoop 25s linear infinite !important;
                 transform: none !important;
                 transition: none !important;
             }
